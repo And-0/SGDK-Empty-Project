65d265147ca0938aa266ef84eba25f78a2cfda7a
And-0 <andrej@ohsat.com>
2019-09-04 02:03:25 +0200

    First Commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <genesis.h>
+#include <resources.h>
+
+int main()
+{
+	//Do cool stuff!
+
+	while(1)
+	{
+		//Do cool stuff in a loop!
+		VDP_waitVSync();
+	}
+
+	//Stop doing cool stuff :(
+	return(0);
+}
+
